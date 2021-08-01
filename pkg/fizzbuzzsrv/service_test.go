d77f053316ed5680700e930f7b50228c5713346f
freak <artback@protonmail.com>
2021-08-01 13:22:53 +0200

    Add benchmarks

--- pkg/fizzbuzzsrv/service_test.go
+++ pkg/fizzbuzzsrv/service_test.go
@@ -72,6 +72,60 @@ func TestService_Get(t *testing.T) {
 	}
 }
 
+func BenchmarkService_Get(b *testing.B) {
+	type args struct {
+		request   *fizzbuzz.FizzBuzzServiceGetRequest
+		sendError error
+	}
+	type expect struct {
+		arg1  interface{}
+		times int
+	}
+	tests := []struct {
+		name string
+		args
+		expect  expect
+		want    *[]string
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				request: &fizzbuzz.FizzBuzzServiceGetRequest{Int1: 2, Int2: 3, Limit: 5, Str1: "fizz", Str2: "buzz"},
+			},
+			expect: expect{
+				arg1: fizz.BuzzValues{
+					Int1: 2, Int2: 3, Limit: 5, Str1: "fizz", Str2: "buzz",
+				},
+			},
+			want: &[]string{
+				"1", "fizz", "buzz", "fizz", "5",
+			},
+		},
+		{
+			name: "error validate",
+			args: args{
+				request: &fizzbuzz.FizzBuzzServiceGetRequest{Int1: 1, Int2: 1, Limit: 5, Str1: "fizz", Str2: "buzz"},
+			},
+			wantErr: true,
+		},
+	}
+	ctrl := gomock.NewController(b)
+	defer ctrl.Finish()
+	for _, tt := range tests {
+		stats := mocks.NewMockStatistics(ctrl)
+		s := Service{Statistics: stats}
+		stats.EXPECT().UpdateStats(gomock.Any(),
+			tt.expect.arg1,
+		).AnyTimes()
+		b.Run(tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, _ = s.Get(context.Background(), tt.args.request)
+			}
+		})
+	}
+}
+
 func TestService_Stats(t *testing.T) {
 	type Return struct {
 		v         interface{}
@@ -116,6 +170,50 @@ func TestService_Stats(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Stats() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkService_Stats(b *testing.B) {
+	type Return struct {
+		v         interface{}
+		frequency uint64
+		err       error
+	}
+	tests := []struct {
+		name    string
+		ret     Return
+		want    *fizzbuzz.FizzBuzzServiceStatsResponse
+		wantErr bool
+	}{
+		{
+			name: "success",
+			want: &fizzbuzz.FizzBuzzServiceStatsResponse{
+				Int1: 1, Int2: 2, Requests: 1,
+			},
+			ret: Return{v: fizz.BuzzValues{Int1: 1, Int2: 2}, frequency: 1, err: nil},
+		},
+		{
+			name:    "bad type assertion",
+			ret:     Return{v: Return{}, frequency: 1, err: nil},
+			wantErr: true,
+		},
+		{
+			name:    "bad type assertion",
+			ret:     Return{v: fizz.BuzzValues{Int1: 1, Int2: 2}, frequency: 1, err: errors.New("something happened")},
+			wantErr: true,
+		},
+	}
+	mockCtrl := gomock.NewController(b)
+	defer mockCtrl.Finish()
+	for _, tt := range tests {
+		stats := mocks.NewMockStatistics(mockCtrl)
+		stats.EXPECT().GetMostUsed(gomock.Any()).AnyTimes().Return(tt.ret.v, tt.ret.frequency, tt.ret.err)
+		s := Service{Statistics: stats}
+		b.Run(tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, _ = s.Stats(context.Background(), &fizzbuzz.FizzBuzzServiceStatsRequest{})
 			}
 		})
 	}
--- pkg/memory/statistics_test.go
+++ pkg/memory/statistics_test.go
@@ -2,6 +2,7 @@ package memory
 
 import (
 	"context"
+	"fmt"
 	"github.com/artback/grpc_http_fizzbuzz/pkg/fizz"
 	"reflect"
 	"testing"
@@ -53,6 +54,63 @@ func Test_statistics_GetMostUsed(t *testing.T) {
 	}
 }
 
+func Benchmark_statistics_GetMostUsed(b *testing.B) {
+	type fields struct {
+		values map[interface{}]uint64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{
+			name: "2 values to search",
+			fields: fields{map[interface{}]uint64{
+				fizz.BuzzValues{Int1: 1}: 5,
+				fizz.BuzzValues{Int1: 2}: 3,
+			}},
+		},
+		{
+			name: "500 values to search",
+			fields: fields{
+				values: func() map[interface{}]uint64 {
+					values := make(map[interface{}]uint64)
+					for i := 0; i < 500; i++ {
+						values[fizz.BuzzValues{Int1: uint64(i)}] = uint64(i + 2)
+					}
+					fmt.Println(len(values))
+					return values
+				}(),
+			},
+		},
+		{
+			name: "5000 values to search",
+			fields: fields{
+				values: func() map[interface{}]uint64 {
+					values := make(map[interface{}]uint64)
+					for i := 0; i < 5000; i++ {
+						values[fizz.BuzzValues{Int1: uint64(i)}] = uint64(i + 2)
+					}
+					return values
+				}(),
+			},
+		},
+		{
+			name:   "empty values",
+			fields: fields{map[interface{}]uint64{}},
+		},
+	}
+	for _, tt := range tests {
+		s := statistics{
+			values: tt.fields.values,
+		}
+		b.Run(tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, _, _ = s.GetMostUsed(context.Background())
+			}
+		})
+	}
+}
+
 func Test_statistics_UpdateStats(t *testing.T) {
 	type fields struct {
 		values map[interface{}]uint64
@@ -91,6 +149,42 @@ func Test_statistics_UpdateStats(t *testing.T) {
 	}
 }
 
+func Benchmark_statistics_UpdateStats(b *testing.B) {
+	type fields struct {
+		values map[interface{}]uint64
+	}
+	type args struct {
+		key interface{}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   map[interface{}]uint64
+	}{
+		{
+			name:   "updateStats 1 time",
+			fields: fields{map[interface{}]uint64{}},
+			args: args{
+				key: fizz.BuzzValues{Int1: 1},
+			},
+			want: map[interface{}]uint64{
+				fizz.BuzzValues{Int1: 1}: 1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		s := statistics{
+			values: tt.fields.values,
+		}
+		b.Run(tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				s.UpdateStats(context.Background(), tt.args.key)
+			}
+		})
+	}
+}
+
 func TestNewMemoryStatistics(t *testing.T) {
 	tests := []struct {
 		name string
@@ -105,6 +199,23 @@ func TestNewMemoryStatistics(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := NewMemoryStatistics(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewMemoryStatistics() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkNewMemoryStatistics(b *testing.B) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "create new",
+		},
+	}
+	for _, tt := range tests {
+		b.Run(tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				NewMemoryStatistics()
 			}
 		})
 	}
